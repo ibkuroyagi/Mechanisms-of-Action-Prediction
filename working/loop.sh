caa3f217dda91e147f1d15a71071c41efd338861
ibkuroyagi <ibkuroyagi@gmail.com>
2020-11-03 23:50:11 +0900

    add v007-4

--- working/loop.sh
+++ working/loop.sh
@@ -27,22 +27,22 @@ stage=2
 conf="conf/tuning/node.v${No}.yaml"
 tag="node/v${No}"
 echo "tag:${tag}, conf:${conf}"
-for step in 10000 20000 30000 40000; do
-    checkpoint="checkpoint-${step}steps"
-    sbatch -J "M.${tag}" ./run.sh \
-        --stage "${stage}" \
-        --tag "${tag}_" \
-        --verbose "${verbose}" \
-        --checkpoint "${checkpoint}" \
-        --step "${step}" \
-        --conf "${conf}"
-done
-# checkpoint="best_loss"
-# sbatch -J "M.${tag}" ./run.sh \
-#     --stage "${stage}" \
-#     --tag "${tag}" \
-#     --verbose "${verbose}" \
-#     --checkpoint "${checkpoint}" \
-#     --step "best" \
-#     --conf "${conf}"
+# for step in 10000 20000 30000 40000; do
+#     checkpoint="checkpoint-${step}steps"
+#     sbatch -J "M.${tag}" ./run.sh \
+#         --stage "${stage}" \
+#         --tag "${tag}" \
+#         --verbose "${verbose}" \
+#         --checkpoint "${checkpoint}" \
+#         --step "${step}" \
+#         --conf "${conf}"
+# done
+checkpoint="best_loss"
+sbatch -J "M.${tag}" ./run.sh \
+    --stage "${stage}" \
+    --tag "${tag}_" \
+    --verbose "${verbose}" \
+    --checkpoint "${checkpoint}" \
+    --step "best" \
+    --conf "${conf}"
 # done
